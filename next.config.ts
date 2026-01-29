230a12560053ab2f00de85b23c55871def9f8681
User <user@example.com>
2026-01-29 20:07:37 +0800

    配置部署：添加 gh-pages 并设置 basePath 和 .nojekyll

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
   },
   // If you are deploying to a GitHub Pages Project site (e.g. username.github.io/repo-name),
   // you must uncomment the line below and replace 'repo-name' with your repository name.
-  // basePath: "/repo-name",
+  basePath: "/lino",
 };
 
 export default nextConfig;
